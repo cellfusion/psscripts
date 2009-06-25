93de1c1a793c0c69eb70fc154b19ae5a81aeb0ac
cellfusion <makoto@cellfusion.jp>
2009-06-26 06:10:28 +0900

    改行コードの修正
    git は改行コードが Unix じゃないと駄目っぽい

--- exportSelectedLayer.jsx
+++ exportSelectedLayer.jsx
@@ -1,1 +1,28 @@
-main();function main() {	var doc = activeDocument;	var layer = doc.activeLayer;			var name = layer.name;		var duppedLayer = layer.duplicate();		var targetLayer = duppedLayer.merge();		var layers = targetLayer.linkedLayers;		var bounds = targetLayer.bounds;		targetLayer.translate(-bounds[0], -bounds[1]);	targetLayer.copy();		var newDocRef = app.documents.add(bounds[2], bounds[3], doc.resolution, name, NewDocumentMode.RGB, DocumentFill.TRANSPARENT);	newDocRef.paste();		newDocRef.trim(TrimType.TRANSPARENT, true, true, true, true);			activeDocument = doc;	targetLayer.remove();		activeDocument = newDocRef;}
+main();
+function main() {
+	var doc = activeDocument;
+	var layer = doc.activeLayer;
+		
+	var name = layer.name;
+	
+	var duppedLayer = layer.duplicate();
+	
+	var targetLayer = duppedLayer.merge();
+	
+	var layers = targetLayer.linkedLayers;
+	
+	var bounds = targetLayer.bounds;
+	
+	targetLayer.translate(-bounds[0], -bounds[1]);
+	targetLayer.copy();
+	
+	var newDocRef = app.documents.add(bounds[2], bounds[3], doc.resolution, name, NewDocumentMode.RGB, DocumentFill.TRANSPARENT);
+	newDocRef.paste();
+	
+	newDocRef.trim(TrimType.TRANSPARENT, true, true, true, true);
+		
+	activeDocument = doc;
+	targetLayer.remove();
+	
+	activeDocument = newDocRef;
+}
